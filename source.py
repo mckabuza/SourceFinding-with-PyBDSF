dcef94432cad2dc000198400b6eb50a3719c0b5c
Kabelo Kesebonye <50317483+mckabuza@users.noreply.github.com>
2020-02-17 17:18:25 +0200

    Update source.py

--- source.py
+++ source.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 14 15:04:57 2020
 
-@author: Kabelo
+@author: Kabelo McKabuza
 """
 
 from astropy.io import fits
@@ -13,13 +13,8 @@ rcParams['font.family'] = 'Courier New'
 rcParams['font.size'] = '16'
 
 image_file = 'ds9.fits'
-
 hdu_list = fits.open(image_file)
-
 data = hdu_list[0].data
-
-
 
 plt.imshow(data, cmap = 'gray')
 plt.colorbar()
-
